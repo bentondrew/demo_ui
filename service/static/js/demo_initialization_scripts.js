aa96f5678097e167cb8ca23f3c54368c0bebc721
Benton Drew <benton.s.drew@drewantech.com>
2017-03-24 16:07:02 -0700

    Update javascript file syntax.

--- service/static/js/demo_initialization_scripts.js
+++ service/static/js/demo_initialization_scripts.js
@@ -1,35 +1,35 @@
 function test_demo_initialization_function() {
-  document.getElementById("demo_initialization_intro").innerHTML = "javascript works!"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "javascript works!";
+};
 
 function create_random_matrix_generation_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Random Matrix Generation Service Database"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "Creating Random Matrix Generation Service Database";
+};
 
 function create_random_matrix_generation_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Random Matrix Generation Service Database Tables"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "Creating Random Matrix Generation Service Database Tables";
+};
 
 function drop_random_matrix_generation_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Random Matrix Generation Service Database Tables"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Random Matrix Generation Service Database Tables";
+};
 
 function drop_random_matrix_generation_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Random Matrix Generation Service Database"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Random Matrix Generation Service Database";
+};
 
 function create_matrix_multiplication_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Matrix Multiplication Service Database"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "Creating Matrix Multiplication Service Database";
+};
 
 function create_matrix_multiplication_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Matrix Multiplication Service Database Tables"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "Creating Matrix Multiplication Service Database Tables";
+};
 
 function drop_matrix_multiplication_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Matrix Multiplication Service Database Tables"
-}
+  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Matrix Multiplication Service Database Tables";
+};
 
 function drop_matrix_multiplication_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Matrix Multiplication Service Database"
+  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Matrix Multiplication Service Database";
 }

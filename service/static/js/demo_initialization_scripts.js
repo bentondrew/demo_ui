6c6cba6451f5e73d00a2ce19ce4845a248d1604a
Benton Drew <benton.s.drew@drewantech.com>
2017-03-24 18:45:00 -0700

    Implement ajax requests to random matrix generator and matrix multiplication services to set up databases.

--- service/static/js/demo_initialization_scripts.js
+++ service/static/js/demo_initialization_scripts.js
@@ -1,27 +1,13 @@
 function test_demo_initialization_function() {
   document.getElementById("demo_initialization_intro").innerHTML = "javascript works!";
 }
-function create_random_matrix_generation_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Random Matrix Generation Service Database";
-}
-function create_random_matrix_generation_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Random Matrix Generation Service Database Tables";
-}
-function drop_random_matrix_generation_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Random Matrix Generation Service Database Tables";
-}
-function drop_random_matrix_generation_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Random Matrix Generation Service Database";
-}
-function create_matrix_multiplication_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Matrix Multiplication Service Database";
-}
-function create_matrix_multiplication_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Creating Matrix Multiplication Service Database Tables";
-}
-function drop_matrix_multiplication_tables() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Matrix Multiplication Service Database Tables";
-}
-function drop_matrix_multiplication_database() {
-  document.getElementById("demo_initialization_intro").innerHTML = "Dropping Matrix Multiplication Service Database";
+
+function simple_ajax_request(method, url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = function() {
+    var response = JSON.parse(xhr.responseText);
+    document.getElementById("demo_initialization_intro").innerHTML = "Status code: " + (xhr.status).toString() + ". Response message: " response.status + ".";
+  };
+  xhr.send();
 }

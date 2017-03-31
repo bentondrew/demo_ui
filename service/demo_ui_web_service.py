695d9f2452d71553ed04f5558deaa704ebe3fb88
Benton Drew <benton.s.drew@drewantech.com>
2017-03-30 21:10:53 -0700

    Add routes to forward browser requests to services.

--- service/demo_ui_web_service.py
+++ service/demo_ui_web_service.py
@@ -5,8 +5,8 @@
 from flask import (Flask,
                    render_template,
                    flash,
-                   redirect,
-                   url_for)
+                   url_for,
+                   jsonify)
 from os import urandom
 
 app = Flask(__name__)
@@ -26,3 +26,83 @@ def demo_initialization():
 @app.route('/demo_execution', methods=['GET'])
 def demo_execution():
   return render_template('demo_execution.html')
+
+
+@app.route('random_matrix_generation/create_matrix_database', methods=['PUT'])
+def create_random_matrix_generation_database():
+  try:
+    #  TODO: Send request to random-matrix-generation service
+    return jsonify({'status': 'Successfully created matrix database.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in creating matrix database: {}'
+                   .format(e)}), 500
+
+
+@app.route('random_matrix_generation/create_matrix_tables', methods=['PUT'])
+def create_random_matrix_generation_tables():
+  try:
+    #  TODO: Send request to random-matrix-generation service
+    return jsonify({'status': 'Successfully created matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in creating matrix tables: {}'
+                   .format(e)}), 500
+
+
+@app.route('random_matrix_generation/drop_matrix_database', methods=['DELETE'])
+def drop_random_matrix_generation_database():
+  try:
+    f#  TODO: Send request to random-matrix-generation service
+    return jsonify({'status': 'Successfully dropped matrix database.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in dropping matrix database: {}'
+                   .format(e)}), 500
+
+
+@app.route('random_matrix_generation/drop_matrix_tables', methods=['DELETE'])
+def drop_random_matrix_generation_tables():
+  try:
+    #  TODO: Send request to random-matrix-generation service
+    return jsonify({'status': 'Successfully dropped matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in dropping matrix tables: {}'
+                   .format(e)}), 500
+
+
+@app.route('matrix_multiplication/create_matrix_database', methods=['PUT'])
+def create_matrix_multiplication_database():
+  try:
+    #  TODO: Send request to matrix-multiplication service
+    return jsonify({'status': 'Successfully created matrix database.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in creating matrix database: {}'
+                   .format(e)}), 500
+
+
+@app.route('matrix_multiplication/create_matrix_tables', methods=['PUT'])
+def create_matrix_multiplication_tables():
+  try:
+    #  TODO: Send request to matrix-multiplication service
+    return jsonify({'status': 'Successfully created matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in creating matrix tables: {}'
+                   .format(e)}), 500
+
+
+@app.route('matrix_multiplication/drop_matrix_database', methods=['DELETE'])
+def drop_matrix_multiplication_database():
+  try:
+    f#  TODO: Send request to matrix-multiplication service
+    return jsonify({'status': 'Successfully dropped matrix database.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in dropping matrix database: {}'
+                   .format(e)}), 500
+
+
+@app.route('matrix_multiplication/drop_matrix_tables', methods=['DELETE'])
+def drop_matrix_multiplication_tables():
+  try:
+    #  TODO: Send request to matrix-multiplication service
+    return jsonify({'status': 'Successfully dropped matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in dropping matrix tables: {}'
+                   .format(e)}), 500
--- service/static/js/demo_initialization_scripts.js
+++ service/static/js/demo_initialization_scripts.js
@@ -4,7 +4,7 @@ function test_demo_initialization_function() {
 
 function simple_ajax_request(method, url) {
   var xhr = new XMLHttpRequest();
-  xhr.open(method, 'http://' + url, true);
+  xhr.open(method, url, true);
   xhr.onload = function() {
     var response = JSON.parse(xhr.responseText);
     document.getElementById("demo_initialization_intro").innerHTML = "Status code: " + (xhr.status).toString() + ". Response message: " + response.status + ".";
